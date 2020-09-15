f679cccafacfdabd99750ed7c718bc19b0dba15a
Pitchayanin-28 <69198640+Pitchayanin-28@users.noreply.github.com>
2020-09-15 13:43:12 +0700

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-    int num1, num, x = 1;
+    int number, num, x = 1;
        srand(time(NULL));
         num1 = rand() % 20;
          cout << "-------------------------------------------\n";
@@ -11,16 +11,16 @@ int main()
         {
             cout << " Enter luckynumber  " << x++ << "(1-20) : ";
             cin >> num;
-            if (num1 != num)
+            if (number != num)
             {
                 cout << "This is wrong number\n";
             }
-            else if (num1 == num)
+            else if (number == num)
             {
                 cout << "Lucky!!!!!!\n";
             }
         }
-        while  (num1 != num);
+        while  (number != num);
         cout << "You got 1,000,000 bath!\n";
         cout << "-------------------------------------\n";
         return 0;
